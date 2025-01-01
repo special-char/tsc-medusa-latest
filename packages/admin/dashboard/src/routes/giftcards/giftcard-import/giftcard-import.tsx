9b119a4ff06e9a66b0f60e8f142c912195150780
Nirav Patel <niravpatel280103@gmail.com>
2025-01-01 11:36:50 +0530

    testing

--- packages/admin/dashboard/src/routes/giftcards/giftcard-import/giftcard-import.tsx
+++ packages/admin/dashboard/src/routes/giftcards/giftcard-import/giftcard-import.tsx
@@ -1,3 +1,222 @@
+// import { Button, Heading, Text, toast } from "@medusajs/ui"
+// import { RouteDrawer } from "../../../components/modals"
+// import { useTranslation } from "react-i18next"
+// import { useMemo, useState } from "react"
+// import { UploadImport } from "./components/upload-import"
+// import { Trash } from "@medusajs/icons"
+// import { FilePreview } from "../../../components/common/file-preview"
+// import { getProductImportCsvTemplate } from "./helpers/import-template"
+// import { useBulkbuyContext } from "../../../providers/bulkbuy-provider"
+
+// export const GiftCardImport = () => {
+//   const { t } = useTranslation()
+
+//   return (
+//     <RouteDrawer>
+//       <RouteDrawer.Header>
+//         <RouteDrawer.Title asChild>
+//           <Heading>{t("giftCards.import.header")}</Heading>
+//         </RouteDrawer.Title>
+//       </RouteDrawer.Header>
+//       <ProductImportContent />
+//     </RouteDrawer>
+//   )
+// }
+
+// const ProductImportContent = () => {
+//   const { t } = useTranslation()
+//   const { setBulkBuyData } = useBulkbuyContext()
+//   const [file, setFile] = useState<File>()
+//   console.log(file?.name, "uploaded file")
+
+//   const productImportTemplateContent = useMemo(() => {
+//     return getProductImportCsvTemplate()
+//   }, [])
+
+//   const handleUploaded = async (file: File) => {
+//     setFile(file)
+//   }
+
+//   const csvToJson = (csv: string): any[] => {
+//     const lines = csv.split("\n").map((line) => line.trim())
+//     const headers = lines[0]?.split(",")
+
+//     if (!headers) {
+//       toast.error("Invalid CSV format")
+//       return []
+//     }
+
+//     const requiredFields = [
+//       "product_handle",
+//       "title",
+//       "unit_price",
+//       "quantity",
+//       "email",
+//     ]
+//     const result = []
+
+//     lines.slice(1).forEach((line, rowIndex) => {
+//       if (line && line.split(",").some((value) => value.trim() !== "")) {
+//         const values = line.split(",")
+//         const record = headers.reduce(
+//           (acc, header, index) => {
+//             acc[header] = values[index]?.trim() || ""
+//             return acc
+//           },
+//           {} as Record<string, string>
+//         )
+
+//         const missingFields = requiredFields.filter((field) => !record[field])
+//         if (missingFields.length > 0) {
+//           throw new Error(
+//             `Row ${rowIndex + 2}: Missing fields: ${missingFields.join(", ")}`
+//           )
+//         } else {
+//           result.push(record)
+//         }
+//       }
+//     })
+
+//     return result
+//   }
+
+//   const handleConfirm = async () => {
+//     try {
+//       if (!file) {
+//         toast.error("No file uploaded")
+//         return
+//       }
+
+//       const readFile = (file: File): Promise<string> => {
+//         return new Promise((resolve, reject) => {
+//           const reader = new FileReader()
+//           reader.onload = (event) => resolve(event.target?.result as string)
+//           reader.onerror = (error) => reject(error)
+//           reader.readAsText(file)
+//         })
+//       }
+
+//       const fileContent = await readFile(file)
+
+//       if (file.type !== "text/csv") {
+//         toast.error("Please upload a valid CSV file")
+//         return
+//       }
+
+//       const convertedJson = csvToJson(fileContent)
+//       console.log(convertedJson, "Converted JSON")
+
+//       const transformedItems = convertedJson?.map((item) => ({
+//         product_handle: item?.product_handle,
+//         title: item?.title,
+//         quantity: parseInt(item?.quantity, 10),
+//         unit_price: parseFloat(item?.unit_price),
+//         metadata: {
+//           firstname: item?.firstname,
+//           lastname: item?.lastname,
+//           email: item?.email,
+//           phone: item?.phone,
+//         },
+//       }))
+
+//       console.log(transformedItems, "Transformed Items")
+
+//       const payload = {
+//         items: transformedItems,
+//         orderData: { currency_code: "mur" },
+//       }
+
+//       setBulkBuyData(transformedItems)
+//       console.log({ payload })
+
+//       // Uncomment to send the payload
+//       // const res = await fetch(`${__BACKEND_URL__}/admin/bulk-order`, {
+//       //   method: "POST",
+//       //   credentials: "include",
+//       //   headers: {
+//       //     "Content-Type": "application/json",
+//       //   },
+//       //   body: JSON.stringify(payload),
+//       // })
+//       // console.log({ res })
+//     } catch (error) {
+//       console.error("Error processing file:", error)
+//       toast.error(
+//         error instanceof Error
+//           ? error.message
+//           : "Failed to process the uploaded file"
+//       )
+//     }
+//   }
+
+//   const uploadedFileActions = [
+//     {
+//       actions: [
+//         {
+//           label: t("actions.delete"),
+//           icon: <Trash />,
+//           onClick: () => setFile(undefined),
+//         },
+//       ],
+//     },
+//   ]
+
+//   return (
+//     <>
+//       <RouteDrawer.Body>
+//         <Heading level="h2">{t("giftCards.import.upload.title")}</Heading>
+//         <Text size="small" className="text-ui-fg-subtle">
+//           {t("giftCards.import.upload.description")}
+//         </Text>
+
+//         <div className="mt-4">
+//           {file?.name ? (
+//             <FilePreview
+//               filename={file?.name}
+//               // loading={isPending}
+//               activity={t("giftCards.import.upload.preprocessing")}
+//               actions={uploadedFileActions}
+//             />
+//           ) : (
+//             <UploadImport onUploaded={handleUploaded} />
+//           )}
+//         </div>
+
+//         {/* {data?.summary && !!filename && (
+//           <div className="mt-4">
+//             <ImportSummary summary={data?.summary} />
+//           </div>
+//         )} */}
+
+//         <Heading className="mt-6" level="h2">
+//           {t("giftCards.import.template.title")}
+//         </Heading>
+//         <Text size="small" className="text-ui-fg-subtle">
+//           {t("giftCards.import.template.description")}
+//         </Text>
+//         <div className="mt-4">
+//           <FilePreview
+//             filename={"product-import-template.csv"}
+//             url={productImportTemplateContent}
+//           />
+//         </div>
+//       </RouteDrawer.Body>
+//       <RouteDrawer.Footer>
+//         <div className="flex items-center gap-x-2">
+//           <RouteDrawer.Close asChild>
+//             <Button size="small" variant="secondary">
+//               {t("actions.cancel")}
+//             </Button>
+//           </RouteDrawer.Close>
+//           <Button onClick={handleConfirm} size="small" disabled={!file?.name}>
+//             {t("actions.import")}
+//           </Button>
+//         </div>
+//       </RouteDrawer.Footer>
+//     </>
+//   )
+// }
+
 import { Button, Heading, Text, toast } from "@medusajs/ui"
 import { RouteDrawer, useRouteModal } from "../../../components/modals"
 import { useTranslation } from "react-i18next"
@@ -29,6 +248,10 @@ const ProductImportContent = () => {
   const [file, setFile] = useState<File>()
   console.log(file?.name, "uploaded file")
 
+  // const { mutateAsync: importProducts, isPending, data } = useImportProducts()
+  // const { mutateAsync: confirm } = useConfirmImportProducts()
+  // const { handleSuccess } = useRouteModal()
+
   const productImportTemplateContent = useMemo(() => {
     return getProductImportCsvTemplate()
   }, [])
@@ -46,38 +269,16 @@ const ProductImportContent = () => {
       return []
     }
 
-    const requiredFields = [
-      "product_handle",
-      "title",
-      "unit_price",
-      "quantity",
-      "email",
-    ]
-    const result = [] as any
-
-    lines.slice(1).forEach((line, rowIndex) => {
-      if (line && line.split(",").some((value) => value.trim() !== "")) {
-        const values = line.split(",")
-        const record = headers.reduce(
-          (acc, header, index) => {
-            acc[header] = values[index]?.trim() || ""
-            return acc
-          },
-          {} as Record<string, string>
-        )
-
-        const missingFields = requiredFields.filter((field) => !record[field])
-        if (missingFields.length > 0) {
-          throw new Error(
-            `Row ${rowIndex + 2}: Missing fields: ${missingFields.join(", ")}`
-          )
-        } else {
-          result.push(record)
-        }
-      }
+    return lines.slice(1).map((line) => {
+      const values = line.split(",")
+      return headers.reduce(
+        (acc, header, index) => {
+          acc[header] = values[index] || ""
+          return acc
+        },
+        {} as Record<string, string>
+      )
     })
-
-    return result
   }
 
   const handleConfirm = async () => {
@@ -103,32 +304,16 @@ const ProductImportContent = () => {
         return
       }
 
-      const convertedJson = csvToJson(fileContent)
-      console.log(convertedJson, "Converted JSON")
-
-      const transformedItems = convertedJson?.map((item) => ({
-        product_handle: item?.product_handle,
-        title: item?.title,
-        quantity: parseInt(item?.quantity, 10),
-        unit_price: parseFloat(item?.unit_price),
-        metadata: {
-          firstname: item?.firstname,
-          lastname: item?.lastname,
-          email: item?.email,
-          phone: item?.phone,
-        },
-      }))
-
-      console.log(transformedItems, "Transformed Items")
+      const convertedjson = csvToJson(fileContent)
+      console.log(convertedjson, "Converted JSON")
 
       const payload = {
-        items: transformedItems,
+        items: convertedjson,
         orderData: { currency_code: "mur" },
       }
 
       console.log({ payload })
 
-      // Uncomment to send the payload
       const res = await fetch(`${__BACKEND_URL__}/admin/bulk-order`, {
         method: "POST",
         credentials: "include",
@@ -140,11 +325,7 @@ const ProductImportContent = () => {
       console.log({ res })
     } catch (error) {
       console.error("Error processing file:", error)
-      toast.error(
-        error instanceof Error
-          ? error.message
-          : "Failed to process the uploaded file"
-      )
+      toast.error("Failed to process the uploaded file")
     }
   }
 
