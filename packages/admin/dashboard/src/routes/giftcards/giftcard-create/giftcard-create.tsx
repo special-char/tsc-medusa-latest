36989a883acae5d77065a0052f5aa030a2ef685a
Yagnesh Modh <contact@thespecialcharacter.com>
2025-01-03 17:32:52 +0530

    commit code

--- packages/admin/dashboard/src/routes/giftcards/giftcard-create/giftcard-create.tsx
+++ packages/admin/dashboard/src/routes/giftcards/giftcard-create/giftcard-create.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom"
 import { useStore } from "../../../hooks/api"
 import { useMemo } from "react"
 import DynamicForm from "../../../components/custom/components/form/DynamicForm"
-import { backendUrl } from "../../../lib/client"
 
 const formSchema = {
   title: {
@@ -97,7 +96,7 @@ export const GiftCardCreate = () => {
     let thumbnailUrl: string | null = null
     if (data.thumbnail) {
       formData.append("files", data.thumbnail.file)
-      const response = await fetch(`${backendUrl}/admin/uploads`, {
+      const response = await fetch(`${__BACKEND_URL__}/admin/uploads`, {
         method: "POST",
         body: formData,
         credentials: "include",
@@ -149,7 +148,7 @@ export const GiftCardCreate = () => {
     }
 
     try {
-      const res = await fetch(`${backendUrl}/admin/products`, {
+      const res = await fetch(`${__BACKEND_URL__}/admin/products`, {
         method: "POST",
         credentials: "include",
         headers: {
