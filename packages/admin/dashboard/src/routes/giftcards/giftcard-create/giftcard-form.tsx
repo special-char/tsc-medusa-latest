f41dc94748d6cc349fbda246411cd2a35ce0f164
harzh307 <harzh.patel@gmail.com>
2025-01-07 16:39:08 +0530

    commit code submit error

--- packages/admin/dashboard/src/routes/giftcards/giftcard-create/giftcard-form.tsx
+++ packages/admin/dashboard/src/routes/giftcards/giftcard-create/giftcard-form.tsx
@@ -101,7 +101,6 @@ const GiftcardForm = ({ defaultValues, regions }: Props) => {
       uploadedMedia = (await Promise.all(fileReqs)).flat()
     } catch (error) {
       if (error instanceof Error) {
-        form.reset(defaultValues, {})
         toast.error(error.message)
       }
     }
