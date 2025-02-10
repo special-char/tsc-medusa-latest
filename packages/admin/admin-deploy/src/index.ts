7503e4a8198dab04a2c3be984c78498c80ed39b0
dharmikpatel21 <dharmikjpatel21@gmail.com>
2025-02-10 16:14:53 +0530

    admin deploy hardcode urls

--- packages/admin/admin-deploy/src/index.ts
+++ packages/admin/admin-deploy/src/index.ts
@@ -12,8 +12,8 @@ async function build() {
 
     await bundler.build({
       path: "/",
-      backendUrl: process.env.BACKEND_URL,
-      storefrontUrl: process.env.STOREFRONT_URL,
+      backendUrl: "https://yogateria.medusajs.app",
+      storefrontUrl: "https://yogateria-v2-storefront.vercel.app",
       sources,
       outDir,
       vite: undefined,
