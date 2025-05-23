56f90e99a7b8412eef12d3c5413ec7e7cc6f06a8
jaykanjia <jaykanjia11@gmail.com>
2025-05-23 12:08:15 +0530

    Refactor SyncPrice component to use useMutation for price synchronization

--- packages/admin/dashboard/src/routes/products/product-list/components/sync-price.tsx
+++ packages/admin/dashboard/src/routes/products/product-list/components/sync-price.tsx
@@ -1,17 +1,13 @@
 import { Button, Container } from "@medusajs/ui"
-import { QueryKey, useQuery, UseQueryOptions } from "@tanstack/react-query"
+import { useMutation, UseMutationOptions } from "@tanstack/react-query"
 import { FetchError } from "@medusajs/js-sdk"
 import { sdk } from "../../../../lib/client"
 
 const useSyncProductPrice = (
-  options?: Omit<
-    UseQueryOptions<any, FetchError, any, QueryKey>,
-    "queryFn" | "queryKey"
-  >
+  options?: Omit<UseMutationOptions<any, FetchError, void>, "mutationFn">
 ) => {
-  const { data, ...rest } = useQuery({
-    queryFn: () => sdk.admin.priceSync.sync(),
-    queryKey: ["sync-product-price"],
+  const { data, ...rest } = useMutation({
+    mutationFn: () => sdk.admin.priceSync.sync(),
     ...options,
   })
 
@@ -19,14 +15,16 @@ const useSyncProductPrice = (
 }
 
 const SyncPrice = () => {
-  const { isLoading } = useSyncProductPrice()
+  const { mutate: syncPrice, isPending } = useSyncProductPrice()
 
-  const handleSyncPrice = () => {}
+  const handleSyncPrice = () => {
+    syncPrice()
+  }
 
   return (
     <Container>
       <div className="flex items-center gap-4">
-        <Button onClick={handleSyncPrice} isLoading={isLoading}>
+        <Button onClick={handleSyncPrice} isLoading={isPending}>
           Sync Product Price (Noble Chain)
         </Button>
       </div>
